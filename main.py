f1d9f287279bf2df6460395aa647e3c399235afa
AAsteriskz7 <102915184+AAsteriskz7@users.noreply.github.com>
2024-09-28 21:15:53 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,40 +1,51 @@
-"""
-Code is used to take the input from the discord bot, server link: https://discord.gg/ztpTEAtXY8
-The bot can take two inputs, image input or text input, the image input goes through openCV and
-identifies if it looks like melanoma (skin cancer). The text input takes an address as an input
-and returns a list of nearby hospitals for the user if needed.
-"""
 import os
 import discord
 from dotenv import load_dotenv
+from gps import get_nearest_hospitals
 
 load_dotenv()
 
 IMAGE_FOLDER = r'C:\Users\avsad\OneDrive\Desktop\Programming\onco-bot\images'
-# Function below clears the image folder every time a new image is recieved
+
 def clear_folder(folder_path):
     for filename in os.listdir(folder_path):
         file_path = os.path.join(folder_path, filename)
         os.remove(file_path)
 
-# Turns on the discord bot when file is run, is always checking for text or message input
+# Turns on the discord bot, always check for inputs
 class MyClient(discord.Client):
-    async def on_ready(self): # logging the bot in
+    async def on_ready(self):  # logging the bot in
         print(f'Logged on as {self.user}!')
 
     async def on_message(self, message):
+        if message.author == self.user:
+            return
+
         if message.attachments:
-            # Image input handling
+            # Image input
             clear_folder(IMAGE_FOLDER)
-            
             for attachment in message.attachments:
                 if attachment.filename.endswith(('.png', '.jpg', '.jpeg')):
                     save_path = os.path.join(IMAGE_FOLDER, attachment.filename)
                     await attachment.save(save_path)
-                    await message.channel.send("Hello World!")  # Send "Hello World!" to the channel
+                    await message.channel.send("sent an image!")  # if image is sent, replied in the channel with this.
+
         elif message.content:
-            # Text input handling
+            # Text input 
             print(f"Message from {message.author}: {message.content}")
+
+            # get hospital data
+            address_input = message.content
+            hospitals = get_nearest_hospitals(address_input)
+            
+            if hospitals:
+                response = "Here are the nearest hospitals:\n"
+                for i, hospital in enumerate(hospitals, start=1):
+                    response += f"{i}. {hospital['name']} - {hospital['address']}\n"
+            else:
+                response = "No hospitals found for the given address."
+            
+            await message.channel.send(response)  # Bot replies with hospital info
 
 intents = discord.Intents.default()
 intents.message_content = True
